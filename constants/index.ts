056f4875226df89b0bb7dd79c6ac4b934ab7006a
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-01-10 18:08:09 +0530

    Complete

--- constants/index.ts
+++ constants/index.ts
@@ -92,24 +92,19 @@ export const Socials = [
 ];
 export const Projects = [
   {
-    title: "Modern Nextjs Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/NextWebsite.png",
+    title: "Modish-ai image generator",
+    text: " Built a web app on Azure, integrating ChatGPT and DALL·E 2 for personalized user experiences. Managed architecture for global scalability and security.",
+    src: "/modish.png",
   },
   {
-    title: "Space Themed Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/SpaceWebsite.png",
+    title: "3d Object Portfolio",
+    text: "Explore and interact with stunning 3D objects for a comprehensive understanding. Ensures seamless communication, real-time updates, and responsiveness for an enhanced user experience. ",
+    src: "/portfolio.png",
   },
   {
-    title: "Modern Nextjs Portfolio",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/WebPortfolio.png",
-  },
-  {
-    title: "Matrix themed Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/Matrix.png",
+    title: "Notion- Saas",
+    text: "Elevate your workflow with our advanced Notion SaaS platform, transcending traditional productivity tools for enhanced efficiency and collaboration.",
+    src: "/notion.png",
   },
 ];
 
